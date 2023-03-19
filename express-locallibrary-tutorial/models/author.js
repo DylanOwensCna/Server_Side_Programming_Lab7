ef69f7c624ccfb9659c164bdab55fe351613938f
Dylan Owens <dylan.owens06@ed.cna.nl.ca>
2023-03-19 17:39:36 -0230

    Fixed bookinstance_detail.pug bug Finished delete functionality Finished update functionality

--- express-locallibrary-tutorial/models/author.js
+++ express-locallibrary-tutorial/models/author.js
@@ -1,43 +1,48 @@
-const { DateTime } = require("luxon");
+var mongoose = require("mongoose");
+const { DateTime } = require("luxon"); // for date handling
 
-const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema;
-
-const AuthorSchema = new Schema({
+var AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   family_name: { type: String, required: true, maxLength: 100 },
   date_of_birth: { type: Date },
   date_of_death: { type: Date },
 });
 
-// Virtual for author's full name
+// Virtual for author "full" name.
 AuthorSchema.virtual("name").get(function () {
-  // To avoid errors in cases where an author does not have either a family name or first name
-  // We want to make sure we handle the exception by returning an empty string for that case
-  let fullname = "";
-  if (this.first_name && this.family_name) {
-    fullname = `${this.family_name}, ${this.first_name}`;
-  }
-  if (!this.first_name || !this.family_name) {
-    fullname = "";
-  }
-  return fullname;
+  return this.family_name + ", " + this.first_name;
 });
 
-// Virtual for author's URL
+// Virtual for this author instance URL.
 AuthorSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
-  return `/catalog/author/${this._id}`;
+  return "/catalog/author/" + this._id;
 });
 
-AuthorSchema.virtual("DOB_Formatted").get(function () {
-  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+AuthorSchema.virtual("lifespan").get(function () {
+  var lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  return lifetime_string;
 });
 
-AuthorSchema.virtual("DOD_Formatted").get(function () {
-  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
 });
 
-// Export model
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+});
+
+// Export model.
 module.exports = mongoose.model("Author", AuthorSchema);
